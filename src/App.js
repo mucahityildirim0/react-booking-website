ec88d20912a464a56c0cf0ebaf512d056004dfad
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-08-02 13:10:09 +0300

    create navbar components and add style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from ".//pages/list/List";
 
 function App() {
-  return <div className="App">booking</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<List />} />
+          <Route path="/hotel/:id" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
